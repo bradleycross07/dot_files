852e554ce1d0dbe6bc6196f5cb0a63c9e249e951
bradleycross07 <182443649+bradleycross07@users.noreply.github.com>
2026-09-04 18:00:57 +0100

    switched to snowflakes and re-organised configurartion

---
+++ nixos/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "nixos config";
+
+  inputs = {
+    # stable NixOS release
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+    # unstable packages to select newer version
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # spicetify
+    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, spicetify-nix, ... }:
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        specialArgs = {
+          inherit nixpkgs-unstable spicetify-nix;
+        };
+
+        modules = [
+          ./configuration.nix
+          ./hardware-configuration.nix
+          spicetify-nix.nixosModules.spicetify
+        ];
+      };
+    };
+}
